53db8578e3d92c5cd0c0b6b877f48b486843cc63
ajatprabha <ajat.prabha.leo@gmail.com>
2020-11-13 22:46:21 +0530

    add ConfigMap ownership to controller

--- internal/controllers/darkroom_controller.go
+++ internal/controllers/darkroom_controller.go
@@ -28,6 +28,7 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -68,5 +69,6 @@ func (r *DarkroomReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 func (r *DarkroomReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&deploymentsv1alpha1.Darkroom{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
