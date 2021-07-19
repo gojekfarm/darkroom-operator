af94e77a1a871ada9882dfb1529e2c263b24830e
ajatprabha <ajat.prabha.leo@gmail.com>
2021-07-19 18:48:28 +0530

    add DELETE method

--- internal/api-server/endpoints/darkroom/responder.go
+++ internal/api-server/endpoints/darkroom/responder.go
@@ -1,17 +1,28 @@
 package darkroom
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/emicklei/go-restful/v3"
+	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 func (e *Endpoint) respond(response *restful.Response, f func() error, errMsg string) {
 	if err := f(); err != nil {
+		apiErr := &apiErrors.StatusError{}
+		if errors.As(err, &apiErr) {
+			_ = response.WriteAsJson(Error{
+				Message: errMsg,
+				Err:     apiErr.Status().Message,
+				Code:    int(apiErr.Status().Code),
+			})
+			return
+		}
 		_ = response.WriteAsJson(Error{
 			Message: errMsg,
 			Err:     err.Error(),
-			Code:    http.StatusInternalServerError,
+			Code:    http.StatusFailedDependency,
 		})
 	}
 }
--- internal/api-server/endpoints/darkroom/setup.go
+++ internal/api-server/endpoints/darkroom/setup.go
@@ -1,6 +1,8 @@
 package darkroom
 
 import (
+	"net/http"
+
 	"github.com/emicklei/go-restful/v3"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -13,13 +15,19 @@ func (e *Endpoint) SetupWithWS(ws *restful.WebService) {
 	ws.Route(ws.GET("{namespace}/darkrooms").To(e.list).
 		Param(ws.PathParameter("namespace", "namespace of darkroom instances").DataType("string")).
 		Doc("List of Darkrooms").
-		Returns(200, "OK", &List{}))
+		Returns(http.StatusOK, "OK", &List{}))
 
 	ws.Route(ws.GET("{namespace}/darkrooms/{name}").To(e.get).
 		Param(ws.PathParameter("namespace", "namespace of darkroom instances").DataType("string")).
 		Param(ws.PathParameter("name", "identifier of darkroom instance").DataType("string")).
 		Doc("Get Darkroom Instance").
-		Returns(200, "OK", &Darkroom{}))
+		Returns(http.StatusOK, "OK", &Darkroom{}))
+
+	ws.Route(ws.DELETE("{namespace}/darkrooms/{name}").To(e.delete).
+		Param(ws.PathParameter("namespace", "namespace of darkroom instances").DataType("string")).
+		Param(ws.PathParameter("name", "identifier of darkroom instance").DataType("string")).
+		Doc("Delete Darkroom Instance").
+		Returns(http.StatusNoContent, "NO CONTENT", &Darkroom{}))
 }
 
 func NewEndpoint(client client.Client) *Endpoint {
